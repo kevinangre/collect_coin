7bf682c2965720840a90b2de67fd1a7e90e21e5b
Kevin Agredo <user@users-MacBook-Pro.local>
2018-12-01 18:59:03 -0500

    Initial commit

---
+++ Assets/KeAgScripts/KeAgMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeAgMover : MonoBehaviour
+{
+    public float speed;
+    public LayerMask isGround;
+    public Transform wallHitBox;
+    private bool wallHit;
+    public float wallHitHeight;
+    public float wallHitWidht;
+
+
+    private void FixedUpdate()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+        wallHit = Physics2D.OverlapBox(wallHitBox.position, new Vector2(wallHitWidht, wallHitHeight), 0, isGround);
+        if (wallHit == true)
+        {
+            speed = speed * -1;
+        }
+    }
+}
+        
+    
